641a3fd4add865aff501f0f46b98e778acad5a38
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-11-05 17:19:00 +0100

    inital commit

---
+++ TT_powheg_LHE2EDM_cfg.py
@@ -0,0 +1,60 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.381.2.7 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: LHEtoEDM -s NONE --conditions auto:startup --eventcontent RAWSIM --datatier GEN-SIM --filein file:../MyPowHegBox_v1/test/TT_7TeV-events.lhe --fileout TT_powheg_LHE2EDM.root --python_filename TT_powheg_LHE2EDM_cfg.py --no_exec -n -1 --filetype LHE
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('LHE')
+
+# import of standard configurations
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.Source("LHESource",
+    fileNames = cms.untracked.vstring('file:../MyPowHegBox_v1/test/TT_7TeV-events.lhe')
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.381.2.7 $'),
+    annotation = cms.untracked.string('LHEtoEDM nevts:-1'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('TT_powheg_LHE2EDM.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:startup', '')
+
+# Path and EndPath definitions
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.RAWSIMoutput_step)
+
